1249ad3b3c87de77e9f51327fd75ddc6adc48bc1
meiersc <meiersc19@gmail.com>
2026-09-20 19:47:29 -0400

    Lab

--- src/MaintenenceCost.java
+++ src/MaintenenceCost.java
@@ -1,4 +1,11 @@
-package PACKAGE_NAME;
-
 public class MaintenenceCost {
+    static void main(String[] args) {
+        double FallCost= 100;
+        double WinterCost= 150;
+        double SpringCost= 100;
+        double SummerCost= 150;
+        double TotalCost= FallCost+WinterCost+SpringCost+SummerCost;
+        System.out.println("The total maintenance cost is "+TotalCost);
+
+    }
 }
